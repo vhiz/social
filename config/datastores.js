06c05d0a2151c5a5bfef35485718efeaf9ea810e
victor mgbeahurike <victormgbeahurike82@gmail.com>
2024-08-22 00:54:59 +0100

    "Commented out MongoDB connection URLs and session name in datastores.js and session.js"

--- config/datastores.js
+++ config/datastores.js
@@ -46,6 +46,6 @@ module.exports.datastores = {
      *                                                                          *
      ***************************************************************************/
     adapter: 'sails-mongo',
-    url: 'mongodb://127.0.0.1:27017/timer',
+    // url: 'mongodb://127.0.0.1:27017/timer',
   },
 }
